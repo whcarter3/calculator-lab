45d3a7a07b4c4b2e5e100758d7393fcd2b391f3e
Will Carter <whcarter3@gmail.com>
2014-11-25 20:03:26 -0800

    cleaned up code, and got rid of all global variables

--- js/main.js
+++ js/main.js
@@ -1,29 +1,27 @@
-// puts all buttons into an array with the variable name buttons
-var buttons = document.getElementsByClassName("buttons");
-var display = document.getElementById("display");
-
-//make each button display the value or function associated with the button
-
-buttons[0].onclick = function() { display.value += this.value };
-buttons[1].onclick = function() { display.value += this.value };
-buttons[2].onclick = function() { display.value += this.value };
-buttons[3].onclick = function() { display.value += this.value };
-buttons[4].onclick = function() { display.value += this.value };
-buttons[5].onclick = function() { display.value += this.value };
-buttons[6].onclick = function() { display.value += this.value };
-buttons[7].onclick = function() { display.value += this.value };
-buttons[8].onclick = function() { display.value += this.value };
-buttons[9].onclick = function() { display.value += this.value };
-buttons[10].onclick = function() { display.value += this.value };
-buttons[11].onclick = function() { display.value += this.value };
-//on clicking the C button, it will clear the display
-buttons[12].onclick = function() { display.value = this.value };
-buttons[13].onclick = function() { display.value += this.value};
-// on clicking the equals button, it will evaluate what is on the display
-buttons[14].onclick = function() { display.value = eval(display.value)};
-buttons[15].onclick = function() { display.value += this.value };
-
-
+(function (){
+function calculator() {
+	// puts all buttons into an array with the variable name buttons
+	var buttons = document.getElementsByClassName("buttons");
+	var display = document.getElementById("display");
+	var clear = document.getElementById("clear");
+	var equals = document.getElementById("equal")
+	//make each button display the value or function associated with the button
+	for (i=0; i < buttons.length; i++) {
+		buttons[i].addEventListener('click', function displayValue(){
+			display.value += this.value;
+		});
+	};
+	// clear the display
+	clear.onclick = function() { 
+		display.value = this.value 
+	};
+	//evaluate the display
+	equals.onclick = function() { 
+		display.value = eval(display.value) 
+	};
+};
+var CALCULATOR = new calculator();
+})();
 
 
 
